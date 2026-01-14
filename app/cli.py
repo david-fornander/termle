c5c914a9bf4a28f5dba54a7796c45cdf9dc32ec1
david-fornander <davfor280@gmail.com>
2026-01-14 18:06:27 +0100

    Adding browser functionality

--- app/cli.py
+++ app/cli.py
@@ -6,13 +6,16 @@ from app.commands import search as search_command
 def main():
 
     if len(sys.argv) == 1:
-        print("Help...")
+        print("help.txt")
         return
 
     cmd = sys.argv[1]
 
     if cmd == "search":
-        search_command.run(" ".join(sys.argv[2:]))
+        search_command.run(sys.argv[2:])
+
+    else:
+        search_command.run(sys.argv[1:])
 
 
 if __name__ == "__main__":
--- app/commands/search.py
+++ app/commands/search.py
@@ -1,2 +1,7 @@
+import webbrowser
+
 def run(search_string):
-    print(f"Searching for: \"{search_string}\"")
+    print(f"Searching for: \"{" ".join(search_string)}\"")
+    webbrowser.open_new_tab("https://www.google.se/search?q=" + "+".join(search_string))
+
+#https://www.google.se/search?q=webbrowser+python+module
